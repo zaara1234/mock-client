cdbe91a629812ba3bd7c0407d0c00d837651799c
zaara afida <zafida.student@hsfi.us>
2017-07-14 18:28:23 +0000

    first commit

---
+++ myscript.js
@@ -0,0 +1,10 @@
+$( document ).ready(function() {
+$('#gogo').click(function(){
+    var submit= $(".Maluma").val();
+    if(submit==="Maluma"){
+        alert("You got this!");
+    }else{
+        alert("Try Again!!");
+    }
+});
+});
