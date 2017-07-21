094963461c910e114d77a5ecbac1108905082f83
zaara afida <zafida.student@hsfi.us>
2017-07-21 15:14:06 +0000

    project

--- myscript.js
+++ myscript.js
@@ -1,10 +1,19 @@
-$( document ).ready(function() {
-$('#gogo').click(function(){
-    var submit= $(".Maluma").val();
-    if(submit==="Maluma"){
-        alert("You got this!");
-    }else{
-        alert("Try Again!!");
-    }
+$(document).ready(function() {
+    $('#gogo').click(function() {
+        var submit = $(".Maluma").val();
+        if (submit === "Maluma") {
+            alert("You got this!");
+        }
+        else {
+            alert("Try Again!!");
+        }
+    });
+    $("#submitBTN").click(function(){
+        var name = $("#name").val();
+        var comment = $("#message").val();
+        $("#comments").append("<h1>" +name +"</h1>");
+        $("#comments").append("<p>" + comment +"</p>");
+
 });
 });
+
